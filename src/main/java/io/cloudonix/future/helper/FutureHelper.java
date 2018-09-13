dfad26550d5da441cf33c55a6cbccd0a75a55e7a
Oded Arbel <oded@geek.co.il>
2018-09-13 09:56:38 +0300

    put back a backward compatible method

--- src/main/java/io/cloudonix/future/helper/FutureHelper.java
+++ src/main/java/io/cloudonix/future/helper/FutureHelper.java
@@ -1,3 +1,4 @@
+
 package io.cloudonix.future.helper;
 
 import java.util.concurrent.CompletableFuture;
@@ -9,4 +10,16 @@ public class FutureHelper extends Futures {
 	public static <T> CompletableFuture<T> completedSuccessfully(T value) {
 		return completedFuture(value);
 	}
+
+	/**
+	 * An alias to {@link #failedFuture(Throwable)} that simulates the
+	 * {@link CompletableFuture#completeExceptionally(Throwable)} semantics.
+	 * 
+	 * @param th an {@link Throwable} representing the error that occured
+	 * @return A completion stage that has already completed exceptionally
+	 */
+	public static <V> CompletableFuture<V> completedExceptionally(Throwable th) {
+		return failedFuture(th);
+	}
+
 }
