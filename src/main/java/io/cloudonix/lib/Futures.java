980d8a7a19b1ac6093202d7162d1e009d94f729d
Oded Arbel <oded@geek.co.il>
2021-03-27 10:41:54 +0300

    removed import I no longer use

--- src/main/java/io/cloudonix/lib/Futures.java
+++ src/main/java/io/cloudonix/lib/Futures.java
@@ -7,7 +7,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Objects;
-import java.util.Queue;
 import java.util.Set;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicBoolean;
